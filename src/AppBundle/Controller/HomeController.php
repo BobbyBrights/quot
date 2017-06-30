5237e2a2c3363d9ee9c52614ec528ff8d925fada
yohan morales <yohanmorales@hotmail.com>
2017-06-29 22:05:28 -0300

    product detail

--- src/AppBundle/Controller/HomeController.php
+++ src/AppBundle/Controller/HomeController.php
@@ -10,9 +10,11 @@ class HomeController extends Controller
 {
     public function indexAction()
     {
-      /*return new Response(
-        '<html><body>Hello world</body></html>'
-      );*/
       return $this->render('home/index.html.twig');
+    }
+
+    public function listAction()
+    {
+      return $this->render('home/product-list.html.twig');
     }
 }
