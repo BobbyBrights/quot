1d132c4c97de8368f597ae4a9feeeaab883854ec
yohan morales <yohanmorales@hotmail.com>
2017-07-05 23:03:05 -0300

    product to cart

--- src/AppBundle/Controller/HomeController.php
+++ src/AppBundle/Controller/HomeController.php
@@ -22,4 +22,9 @@ class HomeController extends Controller
     {
       return $this->render('home/product-list-modal.html.twig');
     }
+
+    public function productToCartAction()
+    {
+      return $this->render('home/product-to-cart.html.twig');
+    }
 }
