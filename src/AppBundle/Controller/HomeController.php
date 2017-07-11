c9ef9390e5434e44f61c6742d6d7b185ad546ef8
Eduardo Velasco <evelasco@dlatv.net>
2017-07-11 16:58:48 -0300

    ruta desde parametro - modificación gitignore

--- src/AppBundle/Controller/HomeController.php
+++ src/AppBundle/Controller/HomeController.php
@@ -10,7 +10,7 @@ class HomeController extends Controller
 {
     public function indexAction()
     {
-        $homeJson = file_get_contents('http://dev-quot.pantheonsite.io/modulos-home?a2b4c1d');
+        $homeJson = file_get_contents($this->container->getParameter('json_home'));
         $home = json_decode($homeJson);
         return $this->render('home/index.html.twig', array('home' => $home));
     }
