0a3d4fc2c7a580bc7eec61bebf5522b87e2b32fc
Eduardo Velasco <evelasco@dlatv.net>
2017-07-13 09:07:54 -0300

    js ancla

--- web/bundles/app/js/quot_functions.js
+++ web/bundles/app/js/quot_functions.js
@@ -1,10 +1,7 @@
-//$(document).ready(function() {
-    console.log('holaaa');
-    $('ul li a').click(function(e){				
-        e.preventDefault();
-        var strAncla=$(this).attr('href');
-        $('body,html').stop(true,true).animate({				
-            scrollTop: $(strAncla).offset().top - 118
-        },1000);	
-    });
-//});
+$('ul li a').click(function(e){				
+    e.preventDefault();
+    var strAncla=$(this).attr('href');
+    $('body,html').stop(true,true).animate({				
+        scrollTop: $(strAncla).offset().top - 118
+    },1000);	
+});
