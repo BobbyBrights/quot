09eb60d8653fa51288302aa6be9857455d57517d
Eduardo Velasco <evelasco@dlatv.net>
2017-07-13 00:43:11 -0300

    anclas

---
+++ web/bundles/app/js/quot_functions.js
@@ -0,0 +1,10 @@
+//$(document).ready(function() {
+    console.log('holaaa');
+    $('ul li a').click(function(e){				
+        e.preventDefault();
+        var strAncla=$(this).attr('href');
+        $('body,html').stop(true,true).animate({				
+            scrollTop: $(strAncla).offset().top
+        },1000);	
+    });
+//});
