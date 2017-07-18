09059211facfc52aa01f0743fb8cbf33c6dbe4ee
yohan morales <yohanmorales@hotmail.com>
2017-07-17 23:20:41 -0300

    js functions

--- web/bundles/app/js/quot_functions.js
+++ web/bundles/app/js/quot_functions.js
@@ -1,15 +1,16 @@
-$('ul li a').click(function(e){				
+$('.main-menu-link').click(function(e){
+  	console.log("dude");
     e.preventDefault();
     var strAncla=$(this).attr('href');
-    $('body,html').stop(true,true).animate({				
+    $('body,html').stop(true,true).animate({
         scrollTop: $(strAncla).offset().top - 118
-    },1000);	
+    },1000);
 });
 
-$('.shirt-block.bk-ancla').click(function(e){				
+$('.shirt-block.bk-ancla').click(function(e){
     e.preventDefault();
     var strAncla=$(this).attr('href');
-    $('body,html').stop(true,true).animate({				
+    $('body,html').stop(true,true).animate({
         scrollTop: $(strAncla).offset().top - 118
-    },1000);	
+    },1000);
 });
