9acd53451d4f3d1fb35359d9321637e2a66a8bee
Eduardo Velasco <evelasco@dlatv.net>
2017-08-02 22:41:45 -0300

    compras

--- src/AppBundle/Controller/ServicesController.php
+++ src/AppBundle/Controller/ServicesController.php
@@ -31,24 +31,48 @@ class ServicesController extends Controller
     }
     
     public function savePreOrderAction(Request $request){
+        if (!$this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_FULLY')) {
+            throw $this->createAccessDeniedException();
+        }
+        $user = $this->get('security.token_storage')->getToken()->getUser();
+        $userId = $user->getId();
+        $shirtPreOrder = $this->getDoctrine()->getManager()->getRepository('AppBundle:PurchaseDetail')->findBy(array(
+            'user_id' => $userId,
+            'vid' => $this->get('request')->request->get('vid'),
+        ));
         
-        $purchaseDetail = new PurchaseDetail();
         $purchaseEm = $this->getDoctrine()->getManager();
-        $purchaseDetail->setUserId($this->get('request')->request->get('user_id'));
-        $purchaseDetail->setDescription($this->get('request')->request->get('description'));
-        $purchaseDetail->setTitle($this->get('request')->request->get('title'));
-        $purchaseDetail->setImage($this->get('request')->request->get('shirt'));
-        $purchaseDetail->setValue($this->get('request')->request->get('value'));
-        $purchaseDetail->setVid($this->get('request')->request->get('vid'));
-        $purchaseDetail->setStatus(0);
-        $purchaseEm->persist($purchaseDetail);
-        $purchaseEm->flush();
-        $idPreOrder = $purchaseDetail->getId();
+        if(!empty($shirtPreOrder)){
+            $quantPre = $shirtPreOrder[0]->getQuant(); 
+            $shirtPreOrder[0]->setQuant($quantPre + 1);
+            $idPreOrder = $shirtPreOrder[0]->getId();
+            $purchaseEm->persist($shirtPreOrder[0]);
+            $purchaseEm->flush();
+        } else{        
+            $quant = 1;
+            $purchaseDetail = new PurchaseDetail();
+            $purchaseDetail->setUserId($this->get('request')->request->get('user_id'));
+            $purchaseDetail->setDescription($this->get('request')->request->get('description'));
+            $purchaseDetail->setTitle($this->get('request')->request->get('title'));
+            $purchaseDetail->setImage($this->get('request')->request->get('shirt'));
+            $purchaseDetail->setValue($this->get('request')->request->get('value'));
+            $purchaseDetail->setSize($this->get('request')->request->get('size'));
+            $purchaseDetail->setQuant($quant);
+            $purchaseDetail->setVid($this->get('request')->request->get('vid'));
+            $purchaseDetail->setStatus(0);
+            $purchaseEm->persist($purchaseDetail);
+            $purchaseEm->flush();
+            $idPreOrder = $purchaseDetail->getId();
+        }
         return new Response('salvando preorder ' . $idPreOrder);
     }
     
     public function listProductsOrderAction(Request $request){
-        $userId = $request->query->get('user_id');
+        if (!$this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_FULLY')) {
+            throw $this->createAccessDeniedException();
+        }
+        $user = $this->get('security.token_storage')->getToken()->getUser();
+        $userId = $user->getId();
         $purchaseDetail = $this->getDoctrine()->getManager()->getRepository('AppBundle:PurchaseDetail')->findBy(array('user_id' => $userId));
         $products = array();
         $totalPurhase = 0;
