45b5f46dffbd2f71fe63bfd77e0d6065f8dd0cb0
Eduardo Velasco <evelasco@dlatv.net>
2017-08-05 18:37:33 -0300

    newsletter

--- src/AppBundle/Controller/HomeController.php
+++ src/AppBundle/Controller/HomeController.php
@@ -5,16 +5,18 @@ namespace AppBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Entity\Newsletter;
 
 class HomeController extends Controller
 {
-    public function indexAction()
+    public function indexAction(Request $request)
     {
         //$homeJson = file_get_contents($this->container->getParameter('json_home'));
         $homeJson = file_get_contents('http://dev-quot.pantheonsite.io/modulos-home');
         $home = json_decode($homeJson);
         $form = $this->createFormBuilder()
-            ->add('correo', 'text', array(
+            ->add('email', 'text', array(
                 'required' => true,
                 'attr' => array(
                     'placeholder' => 'dirección de correo',
@@ -28,6 +30,25 @@ class HomeController extends Controller
                 )
             ))
             ->getForm();
+        
+        $form->handleRequest($request);
+        if ($form->isValid()) {
+            $email= $_POST['form']['email'];
+            $newsletter = new Newsletter();
+            $emailExist = $this->getDoctrine()->getManager()->getRepository('AppBundle:Newsletter')->findBy(array('email' => $email));
+            if(!empty($email)){
+                $this->get('session')->getFlashBag()->add(
+                            'error', 'Este correo ya esta suscrito.'
+                    );
+                return $this->redirect($this->generateUrl('home', array()) . '#newsletter');
+                //return $this->redirectToRoute('home');
+            } else {
+                $newsletter->setEmail($email);
+                $newsletterEm = $this->getDoctrine()->getManager();
+                $newsletterEm->persist($newsletter);
+                $newsletterEm->flush();
+            }
+        }
         return $this->render('home/index.html.twig', array('home' => $home, 'form' => $form->createView()));
     }
 
---
+++ src/AppBundle/Entity/Newsletter.php
@@ -0,0 +1,57 @@
+<?php
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="newsletter")
+ */
+class Newsletter
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /** @ORM\Column(name="email", type="string", length=255, unique=true) */
+    protected $email;
+
+    
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     *
+     * @return Newsletter
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+}
